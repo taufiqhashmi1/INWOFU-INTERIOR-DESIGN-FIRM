40f5df997586f74f6fd918faa95ce139c21a4788
Taufiq Hashmi <taufiqhashmi1@gmail.com>
2025-10-09 10:14:59 +0530

    added database file

--- inwofu/inwofu/urls.py
+++ inwofu/inwofu/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('AnasInwofuAdmin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
     path('',views.home, name = "Home"),
     path('project/<slug:project_slug>/', views.project_detail_view, name='project_detail'),
